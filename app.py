df98c63ff38fe0a0e21ec8a9b34ad65ffd94154e
Ali Naki <122255546+AliNaki@users.noreply.github.com>
2024-09-14 14:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def index():
     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
